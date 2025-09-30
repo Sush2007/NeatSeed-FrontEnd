81868cb48e0d90b7a5c6a7e1524bb1f4541c54e9
Soumya Patnaik <patnaiksoumya20@gmail.com>
2025-09-30 15:34:39 +0530

    Added the Login and Signup page for the Admin dashboard

--- Frontend/NeatSeed/src/App.jsx
+++ Frontend/NeatSeed/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import LandingPage from './Components/LandingPage'
-const App = () => {
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from './Components/LandingPage';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+
+function App() {
   return (
-   <>
-   <LandingPage/>
-   </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
