256122b634632a8d202adf3584e51d020f223a1d
Sushmit Kumar Satapathy <sushmitkumarsatapathy@gmail.com>
2025-11-05 00:11:12 +0530

    Update Trucks.jsx

--- src/Components/Trucks.jsx
+++ src/Components/Trucks.jsx
@@ -1,18 +1,10 @@
 import React, { useState } from 'react';
 import { BarChart3, Truck, MapPin, Home, Bell, TrendingUp, Plus, Pencil, Trash2} from 'lucide-react';
-import { Link } from 'react-router-dom';
+
+import Sidebar from './Sidebar';
 
 export default function Trucks() {
-  const [activeMenu, setActiveMenu] = useState('Trucks');
-
-  const menuItems = [
-    { name: 'Dashboard', icon: BarChart3, path: "/dashboard" },
-    { name: 'Trucks', icon: Truck },
-    { name: 'Routes', icon: MapPin, path: "/routes" },
-   
-    { name: 'Notifications ', icon: Bell, path:"/notification" },
-    { name: 'Analytics', icon: TrendingUp, path: "/analytics" }
-  ];
+ 
 
   const trucks = [
     { id: 'TRK001', driver: 'John Smith', route: 'RT001', status: '', lastUpdate: '1/15/2024, 4:00 PM' },
@@ -23,48 +15,9 @@ export default function Trucks() {
   ];
 
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen bg-white">
       {/* Sidebar */}
-      <div className="w-72 bg-white border-r border-gray-200 flex flex-col">
-        {/* Logo */}
-        <div className="p-6 border-b border-gray-200">
-          <div className="flex items-center gap-3">
-            <div className="w-12 h-12 rounded-xl flex items-center justify-center">
-              <div className="w-24 h-24 bg-white rounded-lg flex items-center justify-center">
-               <img src="logo.jpg" alt="" />
-              </div>
-            </div>
-            <span className="text-2xl font-bold text-gray-800">NeatSeed</span>
-          </div>
-        </div>
-
-        {/* Navigation Menu */}
-        <nav className="flex-1 p-4">
-          {menuItems.map((item) => {
-            const Icon = item.icon;
-            const isActive = activeMenu === item.name;
-            
-            return (
-              <Link to={item.path}>
-
-                  <button
-                key={item.name}
-                onClick={() => setActiveMenu(item.name)}
-                className={`w-full flex items-center gap-3 px-4 py-3 mb-2 rounded-lg transition-all ${
-                  isActive
-                    ? 'bg-green-600 text-white font-semibold'
-                    : 'text-gray-600 hover:bg-gray-100'
-                }`}
-              >
-                <Icon className="w-5 h-5" />
-                <span className="text-base">{item.name}</span>
-              </button>
-              </Link>
-            
-            );
-          })}
-        </nav>
-      </div>
+      <Sidebar/>
 
       {/* Main Content */}
       <div className="flex-1 overflow-auto">
